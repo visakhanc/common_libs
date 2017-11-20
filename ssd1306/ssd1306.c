f15a76b6285654526d7d5e2ec216967957b2fcd2
Visakhan C <visakhanc@gmail.com>
2017-11-20 22:51:58 +0530

    OLED-based naming for definitions and functions

--- ssd1306/ssd1306.c
+++ ssd1306/ssd1306.c
@@ -7,37 +7,43 @@
  *	Author: Visakhan
  */
 
+#include <avr/pgmspace.h>
 #include "avr_twi.h"
 #include "ssd1306.h"
 
+
+static const uint8_t * current_font;
+static uint8_t 	font_page;
+static uint8_t 	font_column;
+
 static twi_params_t		twi_params = 
-	{ 	.slave_addr = SSD_I2C_SLA_ADDR };
+	{ 	.slave_addr = OLED_I2C_SLA_ADDR };
 
 
-static uint8_t ssd_init_cmds[] = {
-	SSD_CONTROL_BYTE_CMD_STREAM,
-	SSD_CMD_SET_MUX_RATIO,			/* Set 64 Multiplexer ratio (default) */
+static uint8_t oled_init_cmds[] = {
+	OLED_CONTROL_BYTE_CMD_STREAM,
+	OLED_CMD_SET_MUX_RATIO,			/* Set 64 Multiplexer ratio (default) */
 	0x3F,
-	SSD_CMD_DISPLAY_OFFSET,			/* Set display offset to 0 (default) */
+	OLED_CMD_DISPLAY_OFFSET,			/* Set display offset to 0 (default) */
 	0x00,
-	SSD_CMD_SET_START_LINE(0),		/* Display RAM start line = 0 (default) */
-	SSD_CMD_SET_SEG_REMAP(0),		/* No Remap of segments (default) */
-	SSD_CMD_COM_SCAN_NORMAL,		/* Normal column scan COM0 to COM63 (default) */
-	SSD_CMD_COM_PIN_CONFIG,			/* Set COM pin config: 0x12(default) - Alternate COM pins, no Left/Right remap */
+	OLED_CMD_SET_START_LINE(0),		/* Display RAM start line = 0 (default) */
+	OLED_CMD_SET_SEG_REMAP(0),		/* No Remap of segments (default) */
+	OLED_CMD_COM_SCAN_NORMAL,		/* Normal column scan COM0 to COM63 (default) */
+	OLED_CMD_COM_PIN_CONFIG,			/* Set COM pin config: 0x12(default) - Alternate COM pins, no Left/Right remap */
 	0x12,
-	SSD_CMD_SET_CONTRAST,			/* Set default contrast (0x7F) */
+	OLED_CMD_SET_CONTRAST,			/* Set default contrast (0x7F) */
 	0x7F,
-	SSD_CMD_DISPLAY_NORMAL,			/* Display follows RAM (default) */
-	SSD_CMD_INVERSE_OFF,			/* Inverse display OFF (default) */
-	SSD_CMD_SET_CLOCK,				/* Set clock divider and oscillator frequency: 0x80 (default) */
+	OLED_CMD_DISPLAY_NORMAL,			/* Display follows RAM (default) */
+	OLED_CMD_INVERSE_OFF,			/* Inverse display OFF (default) */
+	OLED_CMD_SET_CLOCK,				/* Set clock divider and oscillator frequency: 0x80 (default) */
 	0x80,
-	SSD_CMD_SET_CHARGE_PUMP,		/* Enable charge pump */
+	OLED_CMD_SET_CHARGE_PUMP,		/* Enable charge pump */
 	0x14,
-	SSD_CMD_DISPLAY_ON				/* Turn on display */
+	OLED_CMD_DISPLAY_ON				/* Turn on display */
 };
-	
 
-static uint8_t ssd1306_send_buf(uint8_t *buf, uint8_t len)
+
+static uint8_t oled_send_buf(uint8_t *buf, uint8_t len)
 {
 	twi_params.tx_buf = buf;
 	twi_params.tx_count = len;
@@ -47,75 +53,180 @@ static uint8_t ssd1306_send_buf(uint8_t *buf, uint8_t len)
 	return 0;
 }
 
-uint8_t ssd1306_command(uint8_t cmd)
+uint8_t oled_command(uint8_t cmd)
 {
 	uint8_t tx_data[2];
 	
-	tx_data[0] = SSD_CONTROL_BYTE_CMD;
+	tx_data[0] = OLED_CONTROL_BYTE_CMD;
 	tx_data[1] = cmd;
-	return ssd1306_send_buf(tx_data, sizeof(tx_data));
+	return oled_send_buf(tx_data, sizeof(tx_data));
 }
 
-uint8_t ssd1306_data(uint8_t data)
+uint8_t oled_data(uint8_t data)
 {
 	uint8_t tx_data[2];
 	
-	tx_data[0] = SSD_CONTROL_BYTE_DATA;
+	tx_data[0] = OLED_CONTROL_BYTE_DATA;
 	tx_data[1] = data;
-	return ssd1306_send_buf(tx_data, sizeof(tx_data));
+	return oled_send_buf(tx_data, sizeof(tx_data));
 }
 
-uint8_t ssd1306_init(void)
+uint8_t oled_init(void)
 {
 	/* Initialize AVR TWI bus */
 	TWI_Init();
 	/* Initialize SSD1306 display */
-	return ssd1306_send_buf(ssd_init_cmds, sizeof(ssd_init_cmds));	
+	return oled_send_buf(oled_init_cmds, sizeof(oled_init_cmds));	
 }
 
-uint8_t ssd1306_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end)
+uint8_t oled_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end)
 {
 	uint8_t tx_buf[9] = {
-		SSD_CONTROL_BYTE_CMD_STREAM,
-		SSD_CMD_SET_ADDR_MODE,	/* Set Horizontal addressing mode */
+		OLED_CONTROL_BYTE_CMD_STREAM,
+		OLED_CMD_SET_ADDR_MODE,	/* Set Horizontal addressing mode */
 		0x00,
-		SSD_CMD_SET_COL_ADDR,	/* Set column start and end address */
+		OLED_CMD_SET_COL_ADDR,	/* Set column start and end address */
 		col_start,
 		col_end,
-		SSD_CMD_SET_PAGE_ADDR,	/* Set page start and end address */
+		OLED_CMD_SET_PAGE_ADDR,	/* Set page start and end address */
 		pages & 0xF,
 		pages >> 4
 	};
 	
-	return ssd1306_send_buf(tx_buf, sizeof(tx_buf));
+	return oled_send_buf(tx_buf, sizeof(tx_buf));
 }
 
 
 /* Activate Page Addressing mode and set Page & Starting Column */
-uint8_t ssd1306_page_mode(uint8_t page, uint8_t col_start)
+uint8_t oled_page_mode(uint8_t page, uint8_t col_start)
 {
 	uint8_t 		tx_data[6];
 	
-	tx_data[0] = SSD_CONTROL_BYTE_CMD_STREAM;
-	tx_data[1] = SSD_CMD_SET_ADDR_MODE;
-	tx_data[2] = SSD_PAGE_ADDRESSING;
-	tx_data[3] = SSD_CMD_SET_PAGE_START(page);
-	tx_data[4] = SSD_CMD_SET_COL_START_LOWER(col_start);
-	tx_data[5] = SSD_CMD_SET_COL_START_HIGHER(col_start >> 4);
+	tx_data[0] = OLED_CONTROL_BYTE_CMD_STREAM;
+	tx_data[1] = OLED_CMD_SET_ADDR_MODE;
+	tx_data[2] = OLED_PAGE_ADDRESSING;
+	tx_data[3] = OLED_CMD_SET_PAGE_START(page);
+	tx_data[4] = OLED_CMD_SET_COL_START_LOWER(col_start);
+	tx_data[5] = OLED_CMD_SET_COL_START_HIGHER(col_start >> 4);
 	
-	return ssd1306_send_buf(tx_data, sizeof(tx_data));
+	return oled_send_buf(tx_data, sizeof(tx_data));
 }
 
 
 /********************** UTILITY FUNCTIONS *************************/
 
-/* Send a page buffer to display
-   NOTE: page_buf[0] should contain the Control byte: SSD_CONTROL_BYTE_DATA_STREAM 
-		 Display data starts at page_buf[1]
-		 size : Size of the whole page_buf
+/* Send a buffer to display
+   NOTE: buf[0] should contain the Control byte: OLED_CONTROL_BYTE_DATA_STREAM 
+		 Display data starts at buf[1]
+		 size : Size of the whole buffer
  */
-uint8_t ssd1306_display_page(uint8_t *page_buf, uint8_t size)
+uint8_t oled_display_data(uint8_t *buf, uint8_t size)
 {
-	return ssd1306_send_buf(page_buf, size);
+	return oled_send_buf(buf, size);
 }
 
+
+
+void oled_set_font(const uint8_t *font_data)
+{
+	current_font = font_data;
+}
+
+void oled_set_position(uint8_t page, uint8_t column)
+{
+	font_page = page;
+	font_column = column;
+}
+
+
+uint8_t oled_putchar(char c)
+{
+	const uint8_t *fontPtr = current_font;
+	uint8_t height = pgm_read_byte(fontPtr++) >> 3;
+	uint8_t width; 
+	uint16_t index;
+	uint8_t firstChar = pgm_read_byte(fontPtr++);
+	uint8_t charCount = pgm_read_byte(fontPtr++);
+	
+	
+	if((c < firstChar) || (c >= (firstChar+charCount))) {
+		return 1;
+	}
+	c -= firstChar;
+	width = pgm_read_byte(fontPtr+c);
+	if(!width) {
+		return 0;
+	}
+	index = 0;
+	for(uint8_t i = 0; i < c; i++) {
+		index += pgm_read_byte(fontPtr+i) * height;
+	}
+	fontPtr += (charCount+index);
+	//oled_horizontal_mode(OLED_PAGE_RANGE(font_page, font_page+height-1), font_column, font_column+width);
+	index = height;
+	while(index--) {
+		oled_page_mode(font_page, font_column);
+		for(uint8_t i = 0; i < width; i++) {
+			oled_data(pgm_read_byte(fontPtr++));
+		}
+		oled_data(0); // 1 pixel empty space between characters
+		font_page++;
+	}
+	/* update position */
+	font_page -= height;
+	font_column += width+1;
+	
+	return 0;
+}
+
+
+uint8_t oled_putstring(char *str)
+{
+	while(*str) {
+		if(oled_putchar(*str++)) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+void oled_number(uint16_t num, uint8_t dotPos)
+{
+	uint8_t i=0;
+	char arr[5] = {};
+	uint16_t p;
+	
+	do {	
+		p = num/10;
+		arr[i++] =  num - p*10;
+		num = p;
+	} while(p);
+	
+	while(i--) {
+		oled_putchar(arr[i]+48);
+		if(dotPos && (dotPos == i) )
+			oled_putchar('.');
+	}
+}
+
+
+/* 	Display 8-bit Numbers	*/
+void oled_number_small(uint8_t num)
+{
+	uint8_t d=0;
+	d = num/100;
+	if(d)
+		oled_putchar(d+48);
+	num -= (d*100);
+	d = num/10;
+	oled_putchar(d+48);
+	oled_putchar( (num%10)+48 );
+
+}
+
+
+uint8_t oled_get_column(void)
+{
+	return font_column;
+}
--- ssd1306/ssd1306.h
+++ ssd1306/ssd1306.h
@@ -12,74 +12,83 @@
 
 
 /* 7-bit Slave address */
-#define SSD_I2C_SLA_ADDR				0x3C	
+#define OLED_I2C_SLA_ADDR				0x3C	
 
 /* Display dimensions */
-#define SSD_DISPLAY_WIDTH				128
-#define SSD_DISPLAY_HEIGHT				64
+#define OLED_DISPLAY_WIDTH				128
+#define OLED_DISPLAY_HEIGHT				64
 
 /* Possible Control bytes */
-#define SSD_CONTROL_BYTE_DATA_STREAM	0x40
-#define SSD_CONTROL_BYTE_DATA			0xC0
-#define SSD_CONTROL_BYTE_CMD_STREAM		0x00
-#define SSD_CONTROL_BYTE_CMD			0x80
+#define OLED_CONTROL_BYTE_DATA_STREAM	0x40
+#define OLED_CONTROL_BYTE_DATA			0xC0
+#define OLED_CONTROL_BYTE_CMD_STREAM	0x00
+#define OLED_CONTROL_BYTE_CMD			0x80
 
 /* Display Addressing modes */
-#define SSD_PAGE_ADDRESSING				0x2
-#define SSD_VERT_ADDRESSING				0x1
-#define SSD_HORIZ_ADDRESSING			0x0
+#define OLED_PAGE_ADDRESSING			0x2
+#define OLED_VERT_ADDRESSING			0x1
+#define OLED_HORIZ_ADDRESSING			0x0
 
 /* Address definitions */
-#define SSD_FIRST_PAGE					0
-#define SSD_LAST_PAGE					7
-#define SSD_PAGE_RANGE_FULL				0x70 	/* Whole(0-7) Page range for Horizontal addressing mode */
-#define SSD_FIRST_COLUMN				0
-#define SSD_LAST_COLUMN					(SSD_DISPLAY_WIDTH-1)
-
+#define OLED_FIRST_PAGE					0
+#define OLED_LAST_PAGE					7
+#define OLED_PAGE_RANGE_FULL			0x70 	/* Whole(0-7) Page range for Horizontal addressing mode */
+#define OLED_FIRST_COLUMN				0
+#define OLED_LAST_COLUMN				(OLED_DISPLAY_WIDTH-1)
+#define OLED_PAGE_RANGE(from, to)		(((to) << 4)|(from))
 
 
 /*************** COMMANDS ******************/
 
 /* Fundamental Commands */
-#define SSD_CMD_SET_CONTRAST			0x81 	/* Set Contrast command: DOUBLE BYTE command - 0x81,A[7:0] (at Reset: 0x7F) */
-#define SSD_CMD_DISPLAY_ENTIRE			0xA5	/* Entire display ON (ignore RAM content)*/
-#define SSD_CMD_DISPLAY_NORMAL			0xA4	/* Display follows RAM contenet <Reset> */
-#define SSD_CMD_INVERSE_ON				0xA7	/* Inverse display: 0:ON, 1:OFF */
-#define SSD_CMD_INVERSE_OFF				0xA6	/* Set to normal display from inverse  (0:OFF, 1:ON)  <Reset> */
-#define SSD_CMD_DISPLAY_ON				0xAF	/* Display ON */
-#define SSD_CMD_DISPLAY_OFF				0xAE	/* Display OFF <Reset> */
+#define OLED_CMD_SET_CONTRAST			0x81 	/* Set Contrast command: DOUBLE BYTE command - 0x81,A[7:0] (at Reset: 0x7F) */
+#define OLED_CMD_DISPLAY_ENTIRE			0xA5	/* Entire display ON (ignore RAM content)*/
+#define OLED_CMD_DISPLAY_NORMAL			0xA4	/* Display follows RAM contenet <Reset> */
+#define OLED_CMD_INVERSE_ON				0xA7	/* Inverse display: 0:ON, 1:OFF */
+#define OLED_CMD_INVERSE_OFF			0xA6	/* Set to normal display from inverse  (0:OFF, 1:ON)  <Reset> */
+#define OLED_CMD_DISPLAY_ON				0xAF	/* Display ON */
+#define OLED_CMD_DISPLAY_OFF			0xAE	/* Display OFF <Reset> */
 
 /* Addressing setting Commands */
-#define SSD_CMD_SET_COL_START_LOWER(x)	((x) & 0xF)				/* Set Lower nibble of Column start address ONLY for Page Addressing */
-#define SSD_CMD_SET_COL_START_HIGHER(x)	(((x) & 0xF) | 0x10)	/* Set Higher nibble of Column start address ONLY for Page Addressing */
-#define SSD_CMD_SET_ADDR_MODE			0x20					/* Set Addressing mode: DOUBLE BYTE command - 0x20, A[1:0] (Reset: A[1:0] = 10b - Page addressing) */
-#define SSD_CMD_SET_COL_ADDR			0x21					/* 3 BYTE COMMAND: Set column start & end address ONLY Horizontal & Vertical addressing mode */
-#define SSD_CMD_SET_PAGE_ADDR			0x22 					/* 3 BYTE COMMAND: Set Page start & end address ONLY Horizoontal & Vertical addressing mode */
-#define SSD_CMD_SET_PAGE_START(x)		(((x) & 0x7) | 0xB0)	/* Set GDDRAM Page start address (Page0-Page7) ONLY for Page addressing mode */
+#define OLED_CMD_SET_COL_START_LOWER(x)	((x) & 0xF)				/* Set Lower nibble of Column start address ONLY for Page Addressing */
+#define OLED_CMD_SET_COL_START_HIGHER(x)	(((x) & 0xF) | 0x10)	/* Set Higher nibble of Column start address ONLY for Page Addressing */
+#define OLED_CMD_SET_ADDR_MODE			0x20					/* Set Addressing mode: DOUBLE BYTE command - 0x20, A[1:0] (Reset: A[1:0] = 10b - Page addressing) */
+#define OLED_CMD_SET_COL_ADDR			0x21					/* 3 BYTE COMMAND: Set column start & end address ONLY Horizontal & Vertical addressing mode */
+#define OLED_CMD_SET_PAGE_ADDR			0x22 					/* 3 BYTE COMMAND: Set Page start & end address ONLY Horizoontal & Vertical addressing mode */
+#define OLED_CMD_SET_PAGE_START(x)		(((x) & 0x7) | 0xB0)	/* Set GDDRAM Page start address (Page0-Page7) ONLY for Page addressing mode */
 
 /* Hardware configuration */
-#define SSD_CMD_SET_START_LINE(x)		(((x) & 0x3F) | 0x40)	/* RAM display start line 0 - 63 (default: 0)*/
-#define SSD_CMD_SET_SEG_REMAP(x)		(((x) & 0x1) | 0xA0)	/* x = 0 (RESET) COL0 to SEG0; x = 1: COL127 to SEG0 */
-#define SSD_CMD_SET_MUX_RATIO			0xA8					/* DOUBLE BYTE command: 0xA8, A[5:0] RESET=0x3F(64MUX) */
-#define SSD_CMD_COM_SCAN_NORMAL			0xC0					/* [RESET] Scan from COM0 to COM[N-1] */
-#define SSD_CMD_COM_SCAN_REMAPPED		0xC8					/* Scan from COM[N-1] to COM0 */
-#define SSD_CMD_DISPLAY_OFFSET			0xD3					/* DOUBLE BYTE command: COM vertical shift (default:0) A[5:0] */
-#define SSD_CMD_COM_PIN_CONFIG			0xDA					/* DOUBLE BYTE command: A[5:4]|0x02 A[5]: COM Left/Right remap enable (Default: 0-disabled) A[4]: 1(default) */
-#define SSD_CMD_SET_CLOCK				0xD5					/* DOUBLE BYTE command: A[3:0] = clock divider (default:0000b=1), A[7:4]=Osc freq (default:1000b) */
-#define SSD_CMD_SET_CHARGE_PUMP			0x8D					/* DOUBLE BYTE command: A[7:4] = 0x14 Enables charge pump */
-#define SSD_CMD_SET_CONTRAST			0x81					/* DOUBLE BYTE command: A[7:0] default = 0x7F */
-#define SSD_CMD_NOP						0xE3			
+#define OLED_CMD_SET_START_LINE(x)		(((x) & 0x3F) | 0x40)	/* RAM display start line 0 - 63 (default: 0)*/
+#define OLED_CMD_SET_SEG_REMAP(x)		(((x) & 0x1) | 0xA0)	/* x = 0 (RESET) COL0 to SEG0; x = 1: COL127 to SEG0 */
+#define OLED_CMD_SET_MUX_RATIO			0xA8					/* DOUBLE BYTE command: 0xA8, A[5:0] RESET=0x3F(64MUX) */
+#define OLED_CMD_COM_SCAN_NORMAL		0xC0					/* [RESET] Scan from COM0 to COM[N-1] */
+#define OLED_CMD_COM_SCAN_REMAPPED		0xC8					/* Scan from COM[N-1] to COM0 */
+#define OLED_CMD_DISPLAY_OFFSET			0xD3					/* DOUBLE BYTE command: COM vertical shift (default:0) A[5:0] */
+#define OLED_CMD_COM_PIN_CONFIG			0xDA					/* DOUBLE BYTE command: A[5:4]|0x02 A[5]: COM Left/Right remap enable (Default: 0-disabled) A[4]: 1(default) */
+#define OLED_CMD_SET_CLOCK				0xD5					/* DOUBLE BYTE command: A[3:0] = clock divider (default:0000b=1), A[7:4]=Osc freq (default:1000b) */
+#define OLED_CMD_SET_CHARGE_PUMP		0x8D					/* DOUBLE BYTE command: A[7:4] = 0x14 Enables charge pump */
+#define OLED_CMD_SET_CONTRAST			0x81					/* DOUBLE BYTE command: A[7:0] default = 0x7F */
+#define OLED_CMD_NOP					0xE3			
 
 
 /* Function declarations */
-uint8_t ssd1306_init(void);
-uint8_t ssd1306_data(uint8_t data);
-uint8_t ssd1306_command(uint8_t cmd);
-uint8_t ssd1306_page_start(uint8_t page, uint8_t column);
-uint8_t ssd1306_line(uint8_t length);
-uint8_t ssd1306_page_mode(uint8_t page, uint8_t col_start);
-uint8_t ssd1306_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end);
-//uint8_t ssd1306_clear_display(void);
-uint8_t ssd1306_display_page(uint8_t *page_buf, uint8_t size);
+uint8_t oled_init(void);
+uint8_t oled_data(uint8_t data);
+uint8_t oled_command(uint8_t cmd);
+uint8_t oled_page_start(uint8_t page, uint8_t column);
+uint8_t oled_line(uint8_t length);
+uint8_t oled_page_mode(uint8_t page, uint8_t col_start);
+uint8_t oled_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end);
+//uint8_t oled_clear_display(void);
+
+/* Utility functions */
+uint8_t oled_display_data(uint8_t *buf, uint8_t size);
+void oled_set_font(const uint8_t *font_data);
+void oled_set_position(uint8_t page, uint8_t column);
+uint8_t oled_putstring(char *str);
+uint8_t oled_putchar(char c);
+void oled_number(uint16_t num, uint8_t dotPos);
+void oled_number_small(uint8_t num);
+uint8_t oled_get_column(void);
 
 #endif
