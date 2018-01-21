a1765ff3d0f9ce3f2f2f424e958ff311a0ec02f8
Visakhan C <visakhanc@gmail.com>
2018-01-21 16:25:40 +0530

    Minor modifications for avr_twi

--- avr_twi/avr_twi.c
+++ avr_twi/avr_twi.c
@@ -22,15 +22,31 @@
 #endif
 
 static volatile uint8_t  twi_status = TWI_STATUS_DONE;
-static twi_params_t *twi_params;
+static twi_params_t		_twi_params; /* Local structure to copy the parameters from caller */
 
 
 /* Initialize TWI module */
 void TWI_Init(void)
 {
-	/* Bit rate */
+	/* TWBR setting for 400kHz SCL clock */
 	RED_LED_OUT();
-	TWBR = 3; // 8 MHz clock will give 200 kHz SCL frequency
+#if F_CPU == 20000000
+	TWBR = 17; // 20MHz crystal => 408kHz
+#elif F_CPU == 16000000
+	TWBR = 3; // 16MHz crystal => 410kHz
+#elif F_CPU == 12000000
+	TWBR = 7; // 12MHz crystal => 413kHz
+#elif F_CPU == 8000000
+	TWBR = 2; // 8MHz (crystal/internal RC) =>  421kHz
+#elif F_CPU == 4000000
+	TWBR = 2; // 4MHz => 210kHz
+#elif F_CPU == 2000000
+	TWBR = 2; // 2MHz => 105kHz
+#elif F_CPU ==  1000000
+	TWBR = 2; // 1MHz => 52kHz
+#else
+	#error "F_CPU value not OK for TWI driver. Please change F_CPU or add to TWI driver"
+#endif
 }
 
 /* TWI transfer API */
@@ -43,7 +59,7 @@ twi_status_t TWI_Master_Transfer(twi_params_t *params)
 	
 	/* Set parameters for the current transfer */
 	twi_status = TWI_STATUS_BUSY;
-	twi_params = params;
+	_twi_params = *params;
 	RED_LED_ON();
 	/* Send start condition */
 	TWCR = _BV(TWINT)|_BV(TWEA)|_BV(TWSTA)|_BV(TWEN)|_BV(TWIE); // Enable interrupt as well 
@@ -54,6 +70,31 @@ twi_status_t TWI_Master_Transfer(twi_params_t *params)
 	return twi_status;
 }
 
+
+/* TWI Non-blocking transfer API (NOT IMPLEMENTED) */
+uint8_t TWI_Master_Transfer_NB(twi_params_t *params)
+{
+	/* Previous transfer complete ? */
+	if(twi_status == TWI_STATUS_BUSY) {
+		return 1;
+	}
+	
+	/* Set parameters for the current transfer */
+	twi_status = TWI_STATUS_BUSY;
+	_twi_params = *params;
+	RED_LED_ON();
+	/* Send start condition */
+	TWCR = _BV(TWINT)|_BV(TWEA)|_BV(TWSTA)|_BV(TWEN)|_BV(TWIE); // Enable interrupt as well 
+	return 0;
+}
+
+
+twi_status_t TWI_Master_Status(void)
+{
+	return twi_status;
+}
+
+
 /* ISR of TWI interrupt */
 ISR(TWI_vect)
 {
@@ -63,19 +104,19 @@ ISR(TWI_vect)
 	switch(twst = TW_STATUS) {
 		case TW_START:  /* start condition is transmitted, now transmit SLA+R/W */
 		case TW_REP_START : /* Repeated start: transmit SLA+R/W */
-			TWDR = (twi_params->slave_addr << 1) | (twi_params->tx_count == 0);
+			TWDR = (_twi_params.slave_addr << 1) | (_twi_params.tx_count == 0);
 			TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE);
 			break;
 			
 		case TW_MT_SLA_ACK: /* SLA+W / data has been transmitted and ACK received : Transmit further data or send START or STOP */
 		case TW_MT_DATA_ACK:
-			if(twi_params->tx_count) { /* Transmit remaining byte(s) */
-				TWDR = *twi_params->tx_buf++;
-				twi_params->tx_count--;
+			if(_twi_params.tx_count) { /* Transmit remaining byte(s) */
+				TWDR = *_twi_params.tx_buf++;
+				_twi_params.tx_count--;
 				TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE);
 			}
 			else {
-				if(twi_params->rx_count) {	
+				if(_twi_params.rx_count) {	
 					TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE)|_BV(TWSTA); /* send START if data to be read */ 
 				}
 				else {
@@ -86,7 +127,7 @@ ISR(TWI_vect)
 			break;
 			
 		case TW_MR_SLA_ACK: /* SLA+R has been transmitted and ACK received : Now receive data byte(s) with ACK or NACK */
-			if(twi_params->rx_count == 1) {
+			if(_twi_params.rx_count == 1) {
 				TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE);  /* Dont ACK if last byte to be received */
 			}
 			else {
@@ -95,14 +136,14 @@ ISR(TWI_vect)
 			break;
 			
 		case TW_MR_DATA_ACK: /* Data byte received and ACK sent: Receive next byte(s) with ACK/NACK */
-			if(twi_params->rx_count == 1) {
+			if(_twi_params.rx_count == 1) {
 				TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE);  /* Dont ACK if last byte to be received */
 			}
 			else {
 				TWCR = _BV(TWINT)|_BV(TWEN)|_BV(TWIE)|_BV(TWEA); /* Receive data with ACK */
 			}
-			*twi_params->rx_buf++ = TWDR;
-			twi_params->rx_count--;
+			*_twi_params.rx_buf++ = TWDR;
+			_twi_params.rx_count--;
 			break;
 			
 		case TW_MR_DATA_NACK: /* Data byte received and no ACK sent: Send STOP to finish transfer */
--- avr_twi/avr_twi.h
+++ avr_twi/avr_twi.h
@@ -27,7 +27,7 @@ typedef struct {
 /* Initialize TWI bus - This will set the clock frequency to 100 kHz */
 void TWI_Init(void);
 
-/* Performs a TWI transfer, based on parameters passed 
+/* Performs a TWI transfer, based on parameters passed and blocks until transfer is finished
  *		The twi_params_t struct should have following members initialized:
  *			slave_addr - 7 bit slave address in [6:0]
  *			tx_buf, tx_count - Valid tx buffer address and count (NULL/0 if no tx)
@@ -36,5 +36,27 @@ void TWI_Init(void);
  *		Returns: Final status of the transfer(0 = success)
  */
 twi_status_t TWI_Master_Transfer(twi_params_t *params);
+
+
+/* (NOT IMPLEMENTED)
+ * Performs a Non-blocking TWI transfer, based on parameters passed.
+ * Before calling, use TWI_Master_Status() to make sure TWI is currently NOT busy
+ *		The twi_params_t struct should have following members initialized:
+ *			slave_addr - 7 bit slave address in [6:0]
+ *			tx_buf, tx_count - Valid tx buffer address and count (NULL/0 if no tx)
+ *			rx_buf, rx_count - Valid rx buffer address and count (NULL/0 if no rx)
+ *
+ *		Returns: 0 = TWI transfer started
+ *				 1 = TWI is busy and transfer is cancelled
+ */
+uint8_t TWI_Master_Transfer_NB(twi_params_t *params);
+
+
+/*	Returns current status of TWI 
+ *	Use this function with non-blocking TWI transfer to know if the previous transfer is done
+ */
+twi_status_t TWI_Master_Status(void);
+
+
 
 
