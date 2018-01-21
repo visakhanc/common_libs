24dd17c1b31d57ba6a1fb26828e6ab398f1b7d61
Visakhan C <visakhanc@gmail.com>
2018-01-21 16:29:32 +0530

    Added graphic utilities to draw lines

--- ssd1306/ssd1306.c
+++ ssd1306/ssd1306.c
@@ -2,9 +2,10 @@
  *	SSD1306.h
  *	
  *	I2C based driver library for SSD1306 based 128x64 OLED display module for AVR MCUs
+ *	This is a simple library with functions to display text and graphic utilities to draw lines
  *
  *	Date: Sept 21, 2017
- *	Author: Visakhan
+ *	Author: Visakhan C
  */
 
 #include <avr/pgmspace.h>
@@ -12,9 +13,13 @@
 #include "ssd1306.h"
 
 
-static const uint8_t * current_font;
-static uint8_t 	font_page;
-static uint8_t 	font_column;
+/**************************** VARIABLES ********************************/
+
+static uint8_t _page_buf[129] = {OLED_CONTROL_BYTE_DATA_STREAM};  /* Buffer to be used for stream data communication */
+static const uint8_t * current_font;	/* Points to the current font data in flash */
+static uint8_t  current_font_height;		/* Font height as number of pages(rows) */
+static uint8_t 	font_page;				/* Next page to be written */
+static uint8_t 	font_column;			/* Next column to be written */
 
 static twi_params_t		twi_params = 
 	{ 	.slave_addr = OLED_I2C_SLA_ADDR };
@@ -42,6 +47,44 @@ static uint8_t oled_init_cmds[] = {
 	OLED_CMD_DISPLAY_ON				/* Turn on display */
 };
 
+
+static uint8_t oled_horizontal_mode_cmd[9] = {
+		OLED_CONTROL_BYTE_CMD_STREAM,
+		OLED_CMD_SET_ADDR_MODE,	/* Set Horizontal addressing mode */
+		0x00,
+		OLED_CMD_SET_COL_ADDR,	/* Set column start and end address */
+		0, // col_start
+		0, // col_end
+		OLED_CMD_SET_PAGE_ADDR,	/* Set page start and end address */
+		0, // page start
+		0  // page end
+};
+
+
+static uint8_t oled_reset_range_cmd[7] = {
+		OLED_CONTROL_BYTE_CMD_STREAM,
+		OLED_CMD_SET_PAGE_ADDR,	/* To Set Row range to the entire enage (ie, 0 to 7) */
+		0,  // page start
+		7,  // page end
+		OLED_CMD_SET_COL_ADDR,  // To Set Column range to the whole line (ie, 0 to 127)
+		0,                      // This is required, if previous 'horizontal' mode used a narrow column range
+		0x7F,                   //  which did not include the starting column of 'this' page mode
+};
+
+
+static uint8_t oled_page_mode_cmd[6] = {
+		OLED_CONTROL_BYTE_CMD_STREAM,
+		OLED_CMD_SET_ADDR_MODE,
+		OLED_PAGE_ADDRESSING,
+		0, //OLED_CMD_SET_PAGE_START()
+		0, //OLED_CMD_SET_COL_START_LOWER()
+		0, //OLED_CMD_SET_COL_START_HIGHER()
+};
+
+
+
+
+/******************************* PRIVATE FUNCTIONS ***********************************/
 
 static uint8_t oled_send_buf(uint8_t *buf, uint8_t len)
 {
@@ -71,6 +114,7 @@ uint8_t oled_data(uint8_t data)
 	return oled_send_buf(tx_data, sizeof(tx_data));
 }
 
+/* Initialize OLED display */
 uint8_t oled_init(void)
 {
 	/* Initialize AVR TWI bus */
@@ -79,58 +123,70 @@ uint8_t oled_init(void)
 	return oled_send_buf(oled_init_cmds, sizeof(oled_init_cmds));	
 }
 
+/* Change to Horizontal addressing mode with specified page and column range */
 uint8_t oled_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end)
 {
-	uint8_t tx_buf[9] = {
-		OLED_CONTROL_BYTE_CMD_STREAM,
-		OLED_CMD_SET_ADDR_MODE,	/* Set Horizontal addressing mode */
-		0x00,
-		OLED_CMD_SET_COL_ADDR,	/* Set column start and end address */
-		col_start,
-		col_end,
-		OLED_CMD_SET_PAGE_ADDR,	/* Set page start and end address */
-		pages & 0xF,
-		pages >> 4
-	};
+	oled_horizontal_mode_cmd[4] = col_start;
+	oled_horizontal_mode_cmd[5] = col_end;
+	oled_horizontal_mode_cmd[7] = pages & 0xF;
+	oled_horizontal_mode_cmd[8] = pages >> 4;
 	
-	return oled_send_buf(tx_buf, sizeof(tx_buf));
+	return oled_send_buf(oled_horizontal_mode_cmd, sizeof(oled_horizontal_mode_cmd));
+}
+
+/* Reset the Page and Column range to the entire display */
+static uint8_t oled_reset_range(void)
+{
+	return oled_send_buf(oled_reset_range_cmd, sizeof(oled_reset_range_cmd));
 }
 
 
 /* Activate Page Addressing mode and set Page & Starting Column */
 uint8_t oled_page_mode(uint8_t page, uint8_t col_start)
 {
-	uint8_t 		tx_data[6];
+	oled_page_mode_cmd[3] = OLED_CMD_SET_PAGE_START(page);
+	oled_page_mode_cmd[4] = OLED_CMD_SET_COL_START_LOWER(col_start);
+	oled_page_mode_cmd[5] = OLED_CMD_SET_COL_START_HIGHER(col_start >> 4);
 	
-	tx_data[0] = OLED_CONTROL_BYTE_CMD_STREAM;
-	tx_data[1] = OLED_CMD_SET_ADDR_MODE;
-	tx_data[2] = OLED_PAGE_ADDRESSING;
-	tx_data[3] = OLED_CMD_SET_PAGE_START(page);
-	tx_data[4] = OLED_CMD_SET_COL_START_LOWER(col_start);
-	tx_data[5] = OLED_CMD_SET_COL_START_HIGHER(col_start >> 4);
-	
-	return oled_send_buf(tx_data, sizeof(tx_data));
+	return oled_send_buf(oled_page_mode_cmd, sizeof(oled_page_mode_cmd));
 }
 
 
 /********************** UTILITY FUNCTIONS *************************/
 
-/* Send a buffer to display
-   NOTE: buf[0] should contain the Control byte: OLED_CONTROL_BYTE_DATA_STREAM 
-		 Display data starts at buf[1]
-		 size : Size of the whole buffer
- */
+
+
 uint8_t oled_display_data(uint8_t *buf, uint8_t size)
 {
 	return oled_send_buf(buf, size);
 }
 
 
+void oled_clear_area(uint8_t page_range, uint8_t col_start, uint8_t col_end)
+{
+	uint8_t i;
+
+	if(col_start > col_end) {
+		return;
+	}
+	for( i = 1; i < sizeof(_page_buf); i++) {
+		_page_buf[i] = 0;
+	}
+	for(i = page_range & 0xF; i <= (page_range >> 4); i++) {
+		oled_page_mode(i, col_start);
+		oled_display_data(_page_buf, (col_end - col_start + 2));
+	}
+
+}
+
 
 void oled_set_font(const uint8_t *font_data)
 {
 	current_font = font_data;
+	current_font_height = pgm_read_byte(current_font) >> 3;
 }
+
+
 
 void oled_set_position(uint8_t page, uint8_t column)
 {
@@ -139,6 +195,14 @@ void oled_set_position(uint8_t page, uint8_t column)
 }
 
 
+
+uint8_t oled_get_column(void)
+{
+	return font_column;
+}
+
+
+#if 0
 uint8_t oled_putchar(char c)
 {
 	const uint8_t *fontPtr = current_font;
@@ -147,7 +211,48 @@ uint8_t oled_putchar(char c)
 	uint16_t index;
 	uint8_t firstChar = pgm_read_byte(fontPtr++);
 	uint8_t charCount = pgm_read_byte(fontPtr++);
-	
+
+	if((c < firstChar) || (c >= (firstChar+charCount))) {
+		return 1;
+	}
+	c -= firstChar;
+	width = pgm_read_byte(fontPtr+c);
+	if(!width) {
+		return 0;
+	}
+	index = 0;
+	for(uint8_t i = 0; i < c; i++) {
+		index += pgm_read_byte(fontPtr+i) * height;
+	}
+	fontPtr += (charCount+index);
+	index = height;
+	while(index--) {
+		oled_page_mode(font_page, font_column);
+		for(uint8_t i = 0; i < width; i++) {
+			oled_data(pgm_read_byte(fontPtr++));
+		}
+		oled_data(0); // 1 pixel empty space between characters
+		font_page++;
+	}
+	/* update position */
+	font_page -= height;
+	font_column += width+1;
+
+	return 0;
+}
+#endif
+
+
+
+uint8_t oled_putchar(char c)
+{
+	const uint8_t *fontPtr = current_font;
+	uint8_t height = pgm_read_byte(fontPtr++) >> 3;
+	uint8_t width;
+	uint16_t index;
+	uint8_t firstChar = pgm_read_byte(fontPtr++);
+	uint8_t charCount = pgm_read_byte(fontPtr++);
+	uint8_t cnt;
 	
 	if((c < firstChar) || (c >= (firstChar+charCount))) {
 		return 1;
@@ -162,22 +267,24 @@ uint8_t oled_putchar(char c)
 		index += pgm_read_byte(fontPtr+i) * height;
 	}
 	fontPtr += (charCount+index);
-	//oled_horizontal_mode(OLED_PAGE_RANGE(font_page, font_page+height-1), font_column, font_column+width);
-	index = height;
-	while(index--) {
-		oled_page_mode(font_page, font_column);
+	oled_horizontal_mode(OLED_PAGE_RANGE(font_page, font_page+height-1), font_column, font_column+width);
+	cnt = 1;
+	while(height--) {
 		for(uint8_t i = 0; i < width; i++) {
-			oled_data(pgm_read_byte(fontPtr++));
+			_page_buf[cnt++] = pgm_read_byte(fontPtr++);
 		}
-		oled_data(0); // 1 pixel empty space between characters
-		font_page++;
+		_page_buf[cnt++] = 0;
 	}
+	oled_display_data(_page_buf, cnt);
+	/* Reset Row and Column range to the entire display, after using horizontal mode */
+	oled_reset_range();
 	/* update position */
-	font_page -= height;
 	font_column += width+1;
 	
+
 	return 0;
 }
+
 
 
 uint8_t oled_putstring(char *str)
@@ -191,27 +298,41 @@ uint8_t oled_putstring(char *str)
 }
 
 
-void oled_number(uint16_t num, uint8_t dotPos)
+
+void oled_number(int16_t num, uint8_t dotPos)
 {
 	uint8_t i=0;
 	char arr[5] = {};
 	uint16_t p;
 	
+	if(num < 0) {
+		num = -num;
+		oled_putchar('-');
+	}
 	do {	
 		p = num/10;
 		arr[i++] =  num - p*10;
 		num = p;
 	} while(p);
-	
+	/* If digits are less than dot position, show enough leading zeroes after adding Decimal point */
+	if(i <= dotPos) {
+		oled_putchar('0');
+		oled_putchar('.');
+		while(dotPos > i) {
+			oled_putchar('0');
+			dotPos--;
+		}
+	}
+	/* Show digits */
 	while(i--) {
 		oled_putchar(arr[i]+48);
-		if(dotPos && (dotPos == i) )
+		if(dotPos && (dotPos == i))
 			oled_putchar('.');
 	}
 }
 
 
-/* 	Display 8-bit Numbers	*/
+
 void oled_number_small(uint8_t num)
 {
 	uint8_t d=0;
@@ -226,7 +347,186 @@ void oled_number_small(uint8_t num)
 }
 
 
-uint8_t oled_get_column(void)
+
+void oled_number_hex(uint16_t num)
 {
-	return font_column;
+	uint8_t shift = 16;
+	uint8_t c;					
+	
+	oled_putchar('0');
+	oled_putchar('x');
+	do {
+		shift -= 4;
+		c = (uint8_t)((num >> shift) & 0xF);
+		if(c > 9) {
+			c -= 10;
+			oled_putchar('A' + c);
+		}
+		else {
+			oled_putchar(c+48);
+		}
+	} while(shift);
+}
+
+
+
+void oled_update_number(struct oled_disp_struct *disp_struct, int16_t num, uint8_t dot_pos)
+{
+	oled_set_position(disp_struct->row_start, disp_struct->col_start);
+	oled_number(num, dot_pos);
+	oled_clear_area(OLED_PAGE_RANGE(disp_struct->row_start, disp_struct->row_start+current_font_height-1), oled_get_column(), disp_struct->current_col);
+	disp_struct->current_col = oled_get_column();
+}
+
+
+
+void oled_update_number_hex(struct oled_disp_struct *disp_struct, uint16_t num)
+{
+	oled_set_position(disp_struct->row_start, disp_struct->col_start);
+	oled_number_hex(num);
+	oled_clear_area(OLED_PAGE_RANGE(disp_struct->row_start, disp_struct->row_start+current_font_height-1), oled_get_column(), disp_struct->current_col);
+	disp_struct->current_col = oled_get_column();
+}
+
+
+
+void oled_update_text(struct oled_disp_struct *disp_struct, char *text)
+{
+	oled_set_position(disp_struct->row_start, disp_struct->col_start);
+	oled_putstring(text);
+	oled_clear_area(OLED_PAGE_RANGE(disp_struct->row_start, disp_struct->row_start+current_font_height-1), oled_get_column(), disp_struct->current_col);
+	disp_struct->current_col = oled_get_column();
+}
+
+
+
+
+/********** Graphic Utilities *************/
+
+
+void oled_horizontal_line(uint8_t x1, uint8_t x2, uint8_t y)
+{
+	uint8_t i;
+	uint8_t page = y >> 3;	/* y/8 */
+
+	/* x1 should be less than or equal to x2 */
+	if(x1 > x2) {
+		i = x1;
+		x1 = x2;
+		x2 = i;
+	}
+	oled_page_mode(page, x1);
+	y -= (page << 3);	/* page*8 */
+	i = 1;
+	do {
+		_page_buf[i++] = (1 << y);
+		x1++;
+	} while(x1 <= x2);
+	oled_display_data(_page_buf, i);
+
+}
+
+
+
+void oled_vertical_line(uint8_t x, uint8_t y1, uint8_t y2)
+{
+	uint8_t pattern;
+	uint8_t page;
+	uint8_t pos;
+
+	/* y1 should be less than or equal to y2 */
+	if(y1 > y2) {
+		pos = y1;
+		y1 = y2;
+		y2 = pos;
+	}
+	page = y1 >> 3;
+	while(y1 <= y2) {
+		pos = y1 - (page<<3);
+		pattern = ~((1 << pos) - 1);
+		oled_page_mode(page, x);
+		if(y2 <= ((page << 3) + 7)) {
+			pos = y2 - (page << 3);
+			pattern &= ((1 << (pos+1)) - 1);
+			oled_data(pattern);
+			break;
+		}
+		oled_data(pattern);
+		page++;
+		y1 = (page << 3);
+	}
+}
+
+
+
+void oled_line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
+{
+	uint16_t m;
+	uint16_t temp;
+	uint8_t len;
+	uint8_t y;
+	uint8_t i;
+	uint8_t c;
+	uint8_t count;
+
+	if(x1 == x2) {	/* Vertical line */
+		oled_vertical_line(x1, y1, y2);
+	}
+	else if(y1 == y2) {	/* Horizontal line */
+		oled_horizontal_line(x1, x2, y1);
+	}
+	else {	/* Line with a slope */
+		/* x1 should be less than or equal to x2 */
+		if(x1 > x2) {
+			i = x1;
+			x1 = x2;
+			x2 = i;
+			i = y1;
+			y1 = y2;
+			y2 = i;
+		}
+		if(y1 > y2) { /* Negative slope */
+			m = ((y1 - y2) <<  8)/(x2 - x1);
+		}
+		else { /* Positive slope */
+			m = ((y2 - y1) << 8)/(x2 - x1);
+		}
+		count = 0;
+		c = y1;
+		len = x2 - x1;
+		for(i = 1; i <= len; i++) {
+			if(c > y2) { /* Negative slope */
+				temp = (c << 8) - m*i;
+			}
+			else {  /* Positive slope */
+				temp = (c << 8) + m*i;
+			}
+			y = (uint8_t)(temp >> 8);
+			if((temp - (y << 8)) > (0x80)) {
+				y++;
+			}
+			if(y == y1) {
+				count++;
+				continue;
+			}
+			if(count > 0) {
+				oled_horizontal_line(x1, x1+count, y1);
+				x1 += count+1;
+			}
+			else {
+				if(c > y2) { /* Negative slope */
+					oled_vertical_line(x1, y1, y+1);
+				}
+				else { /* Positive slope */
+					oled_vertical_line(x1, y1, y-1);
+				}
+				x1++;
+			}
+			y1 = y;
+			count = 0;
+		}
+		if(x1 < x2) {
+			oled_horizontal_line(x1, x2, y);
+		}
+	}
 }
--- ssd1306/ssd1306.h
+++ ssd1306/ssd1306.h
@@ -2,14 +2,16 @@
  *	SSD1306.h
  *	
  *	I2C based driver library for SSD1306 based 128x64 OLED display module for AVR MCUs
+ *  This is a simple library with functions to display text and graphic utilities to draw lines
  *
  *	Date: Sept 13, 2017
- *	Author: Visakhan
+ *	Author: Visakhan C
  */
  
 #ifndef SSD1306_H
 #define SSD1306_H
 
+#include <stdint.h>
 
 /* 7-bit Slave address */
 #define OLED_I2C_SLA_ADDR				0x3C	
@@ -71,24 +73,200 @@
 #define OLED_CMD_NOP					0xE3			
 
 
-/* Function declarations */
+/* Structure to be passed into oled_update_xxx functions */
+struct oled_disp_struct {
+	uint8_t row_start;
+	uint8_t col_start;
+	uint8_t current_col;
+};
+
+
+/********************* Function declarations ***********************/
+
 uint8_t oled_init(void);
 uint8_t oled_data(uint8_t data);
 uint8_t oled_command(uint8_t cmd);
 uint8_t oled_page_start(uint8_t page, uint8_t column);
-uint8_t oled_line(uint8_t length);
+//uint8_t oled_line(uint8_t length);
 uint8_t oled_page_mode(uint8_t page, uint8_t col_start);
-uint8_t oled_horizontal_mode(uint8_t pages, uint8_t col_start, uint8_t col_end);
-//uint8_t oled_clear_display(void);
+uint8_t oled_horizontal_mode(uint8_t page_range, uint8_t col_start, uint8_t col_end);
 
-/* Utility functions */
+
+
+
+/********************* Utility functions *************************/
+
+
+/**
+ * @brief Send a data buffer to display as a data stream
+ * @param buf - Display data starting at buf[1]
+ *              buf[0] should contain the Control byte: OLED_CONTROL_BYTE_DATA_STREAM
+ * @param size - Size of the whole buffer
+ * @return 0 - Success
+ *         1 - Error
+ */
 uint8_t oled_display_data(uint8_t *buf, uint8_t size);
+
+
+/**
+ * @brief Clear an area bounded by a page and column range
+ * @param page_range - Start and end page defined using OLED_PAGE_RANGE() macro
+ * @param col_start - Starting column (0 - 127)
+ * @param col_end - Ending column (0 - 127)
+ */
+void oled_clear_area(uint8_t page_range, uint8_t col_start, uint8_t col_end);
+
+
+/**
+ * @brief Set the font to be used for text
+ * @param font_data - Font data array (font_8pt, font16_pt, font_24pt etc.)
+ */
 void oled_set_font(const uint8_t *font_data);
+
+
+
+/**
+ * @brief Set the next text location
+ * @param page - Page number (0 - 7)
+ * @param column - Column number (0 - 127)
+ */
 void oled_set_position(uint8_t page, uint8_t column);
-uint8_t oled_putstring(char *str);
-uint8_t oled_putchar(char c);
-void oled_number(uint16_t num, uint8_t dotPos);
-void oled_number_small(uint8_t num);
+
+
+/**
+ * @brief Returns the current text location column
+ * @return Column number (0 - 127)
+ */
 uint8_t oled_get_column(void);
+
+
+/**
+ * Clear the entire display
+ */
+#define oled_clear_display()	oled_clear_area(OLED_PAGE_RANGE_FULL, OLED_FIRST_COLUMN, OLED_LAST_COLUMN)
+
+
+
+/**
+ * @brief Writes a character to the current text location
+ * @param c - Character to write
+ * @return 0 - Success
+ *         1 - Error
+ */
+uint8_t oled_putchar(char c);
+
+
+/**
+ * @brief Writes a string at the current text location
+ * @param str - String to write
+ * @return 0 - Success
+ *         1 - Error
+ */
+uint8_t oled_putstring(char *str);
+
+
+
+/**
+ * @brief Writes a 16-bit signed number with optional decimal point
+ * @param num - 16-bit signed number
+ * @param dotPos - Decimal point position from left (0 = No decimal point)
+ */
+void oled_number(int16_t num, uint8_t dotPos);
+
+
+/**
+ * @brief Display positive 8-bit Numbers
+ * @param num - 8-bit number
+ */
+void oled_number_small(uint8_t num);
+
+
+
+/**
+ * @brief Display 16-bit numbers in hexadecimal format
+ * @param num - 16-bit number
+ */
+void oled_number_hex(uint16_t num);
+
+
+
+/**
+ * @brief Write a number in the display area specified by the 'disp_struct' structure
+ *        This function is used to write a number at a predefined location without having to move
+ *        the text location using oled_set_position()
+ *        To display a number using this function, initialize a oled_disp_struct with desired location
+ *                oled_disp_struct oled_num = { .row_start = 6, .col_start = 10 };
+ *        Then, for updating the number every time,
+ *                oled_update_number(&oled_num, 12345, 0);
+ *
+ * @param disp_struct - Structure initialized with starting row(0-7) and column(0-127)
+ * @param num - 16-bit signed number to display
+ * @param dot_pos - Decimal point position (0 - No DP)
+ */
+void oled_update_number(struct oled_disp_struct *num_struct, int16_t num, uint8_t dot_pos);
+
+
+/**
+ * @brief Write a number in Hex format in the display area specified by the 'disp_struct' structure
+ *        This function is used to write a Hex number at a predefined location without having to move
+ *        the text location using oled_set_position()
+ *        To display a number using this function, initialize a oled_disp_struct with desired location
+ *                oled_disp_struct oled_num = { .row_start = 6, .col_start = 10 };
+ *        Then, for updating the number every time,
+ *                oled_update_number_hex(&oled_num, 12345);
+ *
+ * @param disp_struct - Structure initialized with starting row(0-7) and column(0-127)
+ * @param num - 16-bit number to display
+ */
+void oled_update_number_hex(struct oled_disp_struct *num_struct, uint16_t num);
+
+
+/**
+ * @brief Write string in the display area specified by the 'disp_struct' structure
+ *        This function is used to write a string at a predefined location without having to move
+ *        the text location using oled_set_position()
+ *        To display a number using this function, initialize a oled_disp_struct with desired location
+ *                oled_disp_struct oled_text = { .row_start = 6, .col_start = 10 };
+ *        Then, for updating the text every time,
+ *                oled_update_text(&oled_num, "Hello");
+ *
+ * @param disp_struct - Structure initialized with starting row(0-7) and column(0-127)
+ * @param text - String to display
+ */
+void oled_update_text(struct oled_disp_struct *disp_struct, char *text);
+
+
+
+
+/********************** Graphic utilities *************************/
+
+
+/**
+ * @brief Draw a horizontal line between two points (x1,y) and (x2,y)
+ * @param x1 - X-coordinate of one point (0 - 127)
+ * @param x2 - X-coordinate of the other point (0 - 127)
+ * @param y - Common y-coordinate (0 - 63)
+ */
+void oled_horizontal_line(uint8_t x1, uint8_t x2, uint8_t y);
+
+
+/**
+ * @brief Draw a vertical line between two points (x,y1) and (x,y2)
+ * @param x - Common X-coordinate (0 - 127)
+ * @param y1 - Y-coordinate of one point (0 - 63)
+ * @param y2 - Y-coordinate of the other point (0 - 63)
+ */
+void oled_vertical_line(uint8_t x, uint8_t y1, uint8_t y2);
+
+
+/**
+ * @brief Draw a line between two points (x1,y1) and (x2,y2)
+ * @param x1 - X-coordinate of first point (0 - 127)
+ * @param y1 - Y-coordinate of first point (0 - 63)
+ * @param x2 - X-coordinate of second point (0 - 127)
+ * @param y2 - Y-coordinate of second point (0 - 63)
+ */
+void oled_line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2);
+
 
 #endif
